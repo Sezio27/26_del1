adcecc519de0fb51c07df3433f4401e184742484
Jakob <Sezio@gmail.com>
2022-09-28 18:17:05 +0200

    Implemented Player Class

--- src/spil/Player.java
+++ src/spil/Player.java
@@ -1,4 +1,23 @@
 package spil;
 
 public class Player {
+    private int playerNumber;
+    private int points;
+
+    public Player(int playerNumber, int points) {
+        this.playerNumber = playerNumber;
+        this.points = points;
+    }
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public void setPoints(int newPoints) {
+        points = newPoints;
+    }
+
+    public int getPoints(){
+        return points;
+    }
 }
